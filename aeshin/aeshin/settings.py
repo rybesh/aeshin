98ff4b0b010a508cce9e9df590bbd0639b76fc12
Ryan Shaw <ryanshaw@unc.edu>
2014-07-23 19:21:32 +0000

    remove XFrameOptionsMiddleware so we can load in Sakai

--- aeshin/aeshin/settings.py
+++ aeshin/aeshin/settings.py
@@ -54,7 +54,6 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'aeshin.middleware.XUACompatibleMiddleware',
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
