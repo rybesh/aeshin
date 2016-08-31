c35e970b56ca94a2337c89965dbaea1068ec5cc8
Ryan Shaw <ryanshaw@unc.edu>
2016-08-31 17:51:28 +0000

    Improved grade_submissions command.

--- aeshin/courses/management/commands/grade_submissions.py
+++ aeshin/courses/management/commands/grade_submissions.py
@@ -9,19 +9,25 @@ from courses.models import Assignment, Submission
 
 
 def grade_submission(course, username, assignment_slug, grade):
+    print(username, assignment_slug, grade)
     user = User.objects.get(username=username)
     assignment = Assignment.objects.get(course=course, slug=assignment_slug)
-    submission = Submission.objects.get(assignment=assignment, submitter=user)
+    submission, created = Submission.objects.get_or_create(
+        assignment=assignment, submitter=user)
     submission.grade = grade
     submission.save()
+    return created
 
 
 def comment_on_submission(course, username, assignment_slug, comments):
+    print(username, assignment_slug, '%s comments' % len(comments))
     user = User.objects.get(username=username)
     assignment = Assignment.objects.get(course=course, slug=assignment_slug)
-    submission = Submission.objects.get(assignment=assignment, submitter=user)
-    submission.comments = comments.join('\n')
+    submission, created = Submission.objects.get_or_create(
+        assignment=assignment, submitter=user)
+    submission.comments = ''.join(comments)
     submission.save()
+    return created
 
 
 class Command(MyBaseCommand):
@@ -34,25 +40,33 @@ class Command(MyBaseCommand):
             raise CommandError(
                 'Usage: grade_submissions <grade_and_comments_file.txt>')
         course = self.input_course()
-        with open(args[1]) as f:
+        with open(args[0]) as f:
             username = assignment_slug = grade = None
             comments = []
             expecting = 'username'
-            for line in f:
-                l = line.strip()
-                if len(line) == 0:
+            for l in f:
+                if len(l.strip()) == 0:
+                    if expecting == 'comment':
+                        comments.append(l)
                     continue
                 if expecting == 'username':
-                    username = l
+                    username = l.strip()
                     expecting = 'grade'
                 elif expecting == 'grade':
-                    m = re.match(r'^([-a-z]+):\s([0-9]+)/[0-9]+$', l)
-                    assignment_slug = m.group(1)
-                    grade = float(m.group(2))
-                    grade_submission(course, username, assignment_slug, grade)
+                    m = re.match(r'^([-a-z]+):\s([.0-9]+)/[.0-9]+$', l.strip())
+                    if m:
+                        assignment_slug = m.group(1)
+                        grade = float(m.group(2))
+                        grade_submission(
+                            course, username, assignment_slug, grade)
+                    else:
+                        comments.append(l)
+                        expecting = 'comment'
                 elif expecting == 'comment':
-                    if re.match(r'^----+$', l):
+                    if re.match(r'^----+$', l.strip()):
                         comment_on_submission(
                             course, username, assignment_slug, comments)
+                        comments = []
+                        expecting = 'username'
                     else:
                         comments.append(l)
