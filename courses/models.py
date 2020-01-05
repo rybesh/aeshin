7fd44646a82d3aeff641ef6260a72ff7b178d3a0
Ryan Shaw <ryanshaw@unc.edu>
2020-01-05 09:29:47 -0500

    add descriptions to units

--- courses/models.py
+++ courses/models.py
@@ -160,6 +160,7 @@ class Unit(models.Model):
     starts_with = models.OneToOneField(
         'Meeting', related_name='starts_unit', on_delete=models.PROTECT)
     title = models.CharField(max_length=80)
+    description = models.TextField(blank=True)
 
     def __str__(self):
         return self.title
