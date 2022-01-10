69fb2ca2a26424d7bdc81fb66b84c047ddfbfed4
Ryan Shaw <ryanshaw@unc.edu>
2022-01-10 10:50:52 -0500

    simplify adding students

--- courses/management/commands/addstudents.py
+++ courses/management/commands/addstudents.py
@@ -4,40 +4,7 @@ from django.db import transaction
 from django.db.utils import IntegrityError
 from django.utils.crypto import get_random_string
 from courses.management.commands.utils import MyBaseCommand
-from html.parser import HTMLParser
 import csv
-
-
-class TableReader(HTMLParser):
-    def read(self, f):
-        self.tag = None
-        self.data = ''
-        self.keys = []
-        self.rows = []
-        self.keyindex = 0
-        for line in f:
-            self.feed(line.strip())
-        return self.rows
-
-    def handle_starttag(self, tag, attrs):
-        self.tag = tag
-        if tag == 'tr':
-            if len(self.keys) > 0:
-                self.rows.append({})
-                self.keyindex = 0
-
-    def handle_endtag(self, tag):
-        if self.tag == 'th':
-            self.keys.append(self.data)
-        elif self.tag == 'td':
-            key = self.keys[self.keyindex]
-            self.rows[-1][key] = self.data
-            self.keyindex += 1
-        self.tag = None
-        self.data = ''
-
-    def handle_data(self, data):
-        self.data += data
 
 
 class Command(MyBaseCommand):
@@ -45,30 +12,18 @@ class Command(MyBaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('roster')
-        parser.add_argument('emails')
 
     @transaction.atomic
-    def handle(self, *args, **options):
+    def handle(self, *_, **options):
         roster = options['roster']
-        emails = options['emails']
-        if roster.endswith('.csv'):
-            reader = csv.DictReader
-        elif roster.endswith('.xls'):
-            reader = TableReader().read
-        else:
-            raise CommandError(
-                'Student roster must be in CSV or XLS (really HTML).')
-        email_list = []
-        with open(emails) as f:
-            for line in f:
-                email_list.append(line.strip())
+        reader = csv.DictReader
         students = []
         new_count = existing_count = 0
-        for i, row in enumerate(reader(open(roster, 'rt', encoding='utf8'))):
+        for row in reader(open(roster, 'rt', encoding='utf8')):
             try:
                 last_name, first_name = [
                     x.strip() for x in row['Name'].split(',')]
-                email = email_list[i]
+                email = row['Email'].strip()
                 username = email.split('@')[0]
                 try:
                     student, created = User.objects.get_or_create(
@@ -93,9 +48,6 @@ class Command(MyBaseCommand):
                     raise CommandError('Username collision: %s' % username)
             except KeyError as e:
                 raise CommandError('%s is missing a %s value.' % (roster, e))
-        if not len(students) == len(email_list):
-            raise CommandError(
-                'Number of emails does not match number of students.')
         self.stdout.write(
             '%s new students and %s existing students.\n'
             % (new_count, existing_count))
