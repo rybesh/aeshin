cfe7eb6dbfb425bb30f217debe9a032a7ee0428e
Ryan Shaw <ryanshaw@unc.edu>
2022-04-29 14:46:18 -0400

    more robust grade importing

--- courses/management/commands/import_grades.py
+++ courses/management/commands/import_grades.py
@@ -8,6 +8,13 @@ from courses.management.commands.utils import MyBaseCommand
 from courses.models import Submission
 
 
+def get(row, column):
+    try:
+        return row[column]
+    except:
+        return row[column.capitalize()]
+
+
 class Command(MyBaseCommand):
     args = '<grades.csv>'
     help = 'a CSV file of grades.'
@@ -25,18 +32,21 @@ class Command(MyBaseCommand):
             reader = csv.DictReader(csvfile)
             for row in reader:
                 try:
-                    submitter = User.objects.get(username=row['username'])
+                    submitter = User.objects.get(username=get(row, 'username'))
                     submission, created = Submission.objects.get_or_create(
                         assignment=assignment,
                         submitter=submitter,
                     )
                     if str.isdigit(row['grade'].replace('.', '')):
-                        submission.grade = float(row['grade'])
+                        submission.grade = float(get(row, 'grade'))
                         submission.letter_grade = ''
                     else:
                         submission.grade = 0.0
-                        submission.letter_grade = row['grade']
-                    submission.comments = row['comment']
+                        submission.letter_grade = get(row, 'grade')
+                    try:
+                        submission.comments = get(row, 'comment')
+                    except:
+                        submission.comments = ''
                     submission.save()
                 except User.DoesNotExist:  # pylint: disable=E1101
                     raise CommandError('Cannot find user: %s ' % row['username'])
