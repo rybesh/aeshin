b57945a1ab1363413044f8bfd0c21eb875659a0e
Ryan Shaw <ryanshaw@unc.edu>
2022-08-15 13:07:50 -0400

    specify length of random string

--- courses/management/commands/addstudents.py
+++ courses/management/commands/addstudents.py
@@ -8,64 +8,66 @@ import csv
 
 
 class Command(MyBaseCommand):
-    help = 'Creates student accounts and adds them to a course.'
+    help = "Creates student accounts and adds them to a course."
 
     def add_arguments(self, parser):
-        parser.add_argument('roster')
+        parser.add_argument("roster")
 
     @transaction.atomic
     def handle(self, *_, **options):
-        roster = options['roster']
+        roster = options["roster"]
         reader = csv.DictReader
         students = []
         new_count = existing_count = 0
-        for row in reader(open(roster, 'rt', encoding='utf8')):
+        for row in reader(open(roster, "rt", encoding="utf8")):
             try:
-                last_name, first_name = [
-                    x.strip() for x in row['Name'].split(',')]
-                email = row['Email'].strip()
-                username = email.split('@')[0]
+                last_name, first_name = [x.strip() for x in row["Name"].split(",")]
+                email = row["Email"].strip()
+                username = email.split("@")[0]
                 try:
                     student, created = User.objects.get_or_create(
-                        first_name=first_name, last_name=last_name,
-                        defaults={'username': username})
+                        first_name=first_name,
+                        last_name=last_name,
+                        defaults={"username": username},
+                    )
                     if created:
                         self.stdout.write(
-                            'New student: %s %s (%s)\n'
-                            % (first_name, last_name, email))
-                        student.set_password(get_random_string())
+                            "New student: %s %s (%s)\n" % (first_name, last_name, email)
+                        )
+                        student.set_password(get_random_string(32))
                         new_count += 1
                     else:
                         self.stdout.write(
-                            'Old student: %s %s (%s)\n'
-                            % (first_name, last_name, email))
+                            "Old student: %s %s (%s)\n" % (first_name, last_name, email)
+                        )
                         existing_count += 1
                     student.email = email
                     student.is_active = True
                     student.save()
                     students.append(student)
                 except IntegrityError:
-                    raise CommandError('Username collision: %s' % username)
+                    raise CommandError("Username collision: %s" % username)
             except KeyError as e:
-                raise CommandError('%s is missing a %s value.' % (roster, e))
+                raise CommandError("%s is missing a %s value." % (roster, e))
         self.stdout.write(
-            '%s new students and %s existing students.\n'
-            % (new_count, existing_count))
-        self.stdout.write('To which course will these students be added?\n')
+            "%s new students and %s existing students.\n" % (new_count, existing_count)
+        )
+        self.stdout.write("To which course will these students be added?\n")
         course = self.input_course()
         added_count = existing_count = 0
         for student in course.students.all():
             if student not in students:
                 self.stdout.write(
-                    '%s appears to have dropped the course.\n'
-                    % student.email)
+                    "%s appears to have dropped the course.\n" % student.email
+                )
         for student in students:
             if course.has_student(student):
                 existing_count += 1
             else:
                 course.students.add(student)
-                self.stdout.write('%s\n' % student.email)
+                self.stdout.write("%s\n" % student.email)
                 added_count += 1
         self.stdout.write(
-            '%s students added (%s already in that course).\n'
-            % (added_count, existing_count))
+            "%s students added (%s already in that course).\n"
+            % (added_count, existing_count)
+        )
