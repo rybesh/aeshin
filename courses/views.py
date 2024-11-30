3db142acecc127c3b172e3cd54c73886498f26c8
Ryan Shaw <ryanshaw@unc.edu>
2024-11-30 10:38:52 -0500

    set lineterminator to newline when writing csv

--- courses/views.py
+++ courses/views.py
@@ -208,7 +208,7 @@ def grades_csv(course):
                 row.extend(["", ""])
         table.append(row)
     buf = StringIO()
-    csv.writer(buf).writerows(table)
+    csv.writer(buf, lineterminator="\n").writerows(table)
     response = HttpResponse(buf.getvalue(), content_type="text/csv")
     response["Content-Disposition"] = 'attachment; filename="grades.csv"'
     return response
